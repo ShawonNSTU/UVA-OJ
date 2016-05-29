e7aea699ae3c67537ef6184ef7f6d330995026e7
nstu83cube <shuvroshawon9@gmail.com>
2016-05-29 21:09:21 +0000

    Add files via upload

---
+++ sms typing.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+main()
+{
+    char s1[1000],s3[1000];
+    char s2[10][6]={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"," "};
+    int i,j,k,t,l;
+    scanf("%d",&t);
+    for(l=1;l<=t;l++){
+     gets(s1);
+     int sum=0;
+    for(i=0;i<=8;i++){
+        strcpy(s3,s2[i]);
+        for(k=0;s1[k];k++){
+        for(j=0;s3[j];j++){
+            if(s1[k]==s3[j])
+                sum+=(j+1);
+        }
+    }
+}
+    printf("Case #%d: %d\n",l,sum);
+    }
+}
