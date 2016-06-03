f90e16a3f7fc45a2a651a07a531c1e7a18dd34a6
nstu83cube <shuvroshawon9@gmail.com>
2016-06-04 03:26:46 +0600

    Add files via upload

---
+++ bricks.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int stacks,height[100],divide,i,t=0;
+    while(scanf("%d",&stacks)==1&&stacks!=0){
+            ++t;
+            int sum=0;
+        for(i=0;i<stacks;i++){
+            scanf("%d",&height[i]);
+            sum+=height[i];
+        }
+    divide=sum/stacks;
+    int m=0;
+    for(i=0;i<stacks;i++){
+            if(height[i]>divide)
+                m+=height[i]-divide;
+        }
+        printf("Set #%d\nThe minimum number of moves is %d.\n\n",t,m);
+    }
+    return 0;
+}
