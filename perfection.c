a8628664a0450f88fa5338046aa36da82b73cfd6
nstu83cube <shuvroshawon9@gmail.com>
2016-06-11 17:47:31 +0600

    Add files via upload

---
+++ perfection.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    long long int num,i,sum,k=1;
+    while(scanf("%lld",&num)){
+        if(k==1)
+            printf("PERFECTION OUTPUT\n");
+            if(num==0){
+                printf("END OF OUTPUT\n");
+                break;
+            }
+        sum=0;
+        for(i=1;i<=num/2;i++){
+            if(num%i==0)
+                sum+=i;
+    }
+        if(num==sum)
+            printf("%5lld PERFECT\n",num);
+        else if(num>sum)
+            printf("%5lld DEFICIENT\n",num);
+        else if(num<sum)
+            printf("%5lld ABUNDANT\n",num);
+        k++;
+
+    }
+    return 0;
+}
