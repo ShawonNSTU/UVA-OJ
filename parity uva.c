242d49fc9eb0b623b44453ca289b80297c713fe2
shawon.ice.nstu <shuvroshawon9@gmail.com>
2016-07-01 12:56:35 +0600

    Add files via upload

---
+++ parity uva.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    long int num;
+    int k,i,rem,ara[50],count;
+    while(scanf("%ld",&num)&&num!=0){
+            k=0;
+            count=0;
+        while(num>0){
+            rem=num%2;
+            ara[k]=rem;
+            num/=2;
+            k++;
+        }
+        printf("The parity of ");
+        for(i=k-1;i>=0;i--){
+                printf("%d",ara[i]);
+            if(ara[i]==1)
+                count++;
+        }
+        printf(" is %d (mod 2).\n",count);
+    }
+    return 0;
+}
---
+++ prime word.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char digit[52],ch1='a',ch2='A',s[21];
+    int i;
+    for(i=0;i<=25;i++){
+        digit[i]=ch1;
+        digit[i+26]=ch2;
+        ch1++;
+        ch2++;
+    }
+    while(gets(s)){
+        int j,sum=0;
+    for(i=0;s[i];i++){
+        for(j=0;j<=51;j++){
+            if(digit[j]==s[i])
+                sum+=j+1;
+        }
+    }
+    int count=0;
+    for(i=2;i<=sum/2;i++){
+        if(sum%i==0){
+            count=1;
+            break;
+        }
+    }
+    if(count==0)
+        printf("It is a prime word.\n");
+    else
+        printf("It is not a prime word.\n");
+    }
+    return 0;
+}
