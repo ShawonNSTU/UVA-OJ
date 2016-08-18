b9b5e2f8a6270480e415ad01fd344ac942a69e0f
ShawonNSTU <shuvroshawon9@gmail.com>
2016-08-19 00:56:14 +0600

    Add files via upload

---
+++ uva 10127(1).cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+long long bigmod(long long base,int power,int mod);
+int main()
+{
+    int i,number;
+    while(scanf("%d",&number)==1){
+        long long mod=0,c;
+        for(i=0;;i++){
+            c=bigmod(10,i,number);
+            mod=(mod+c)%number;
+            if(mod==0) break;
+        }
+    printf("%d\n",++i);
+    }
+    return 0;
+}
+long long bigmod(long long base,int power,int mod)
+{
+    if(power==0) return 1;
+    if(power%2) return ((base%mod)*bigmod(base,power-1,mod))%mod;
+    else{
+        long long c=bigmod(base,power/2,mod);
+        return ((c%mod)*(c%mod))%mod;
+    }
+}
